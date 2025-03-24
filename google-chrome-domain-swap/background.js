544bfb246e8740d35ec50abf1f3f09d89981754d
Ashish Bachavala <ashish.bachavala@mixpanel.com>
2025-03-24 16:27:43 -0400

    always use popup

--- google-chrome-domain-swap/background.js
+++ google-chrome-domain-swap/background.js
@@ -4,25 +4,30 @@ async function checkForDomainSet(tabId, changeInfo, tab) {
 	var activeSet = await DomainSwitcher.getCurrentSet( tab );
 
 	if( activeSet ) {
-		if( activeSet.domains.length > 2 ) {
-			chrome.action.setPopup({
-				tabId: tabId,
-				popup: "popup.html"
-			});
-		} else {
-			chrome.action.onClicked.addListener(function( tab ) {
-				var hostname = DomainSwitcher.getHostname( tab );
+		chrome.action.setPopup({
+			tabId: tabId,
+			popup: "popup.html"
+		});
+		// if( activeSet.domains.length > 2 ) {
+		// 	chrome.action.setPopup({
+		// 		tabId: tabId,
+		// 		popup: "popup.html"
+		// 	});
+		// } else {
+		// 	chrome.action.onClicked.addListener(function( tab ) {
+		// 		var hostname = DomainSwitcher.getHostname( tab );
+		// 		var set = DomainSwitcher.getCurrentSet( tab );
 
-				activeSet.domains.forEach(function(domain, i) {
-					if( hostname !== domain ) {
-						var newUrl = tab.url.replace(hostname, domain);
+		// 		set.domains.forEach(function(domain, i) {
+		// 			if( hostname !== domain ) {
+		// 				var newUrl = tab.url.replace(hostname, domain);
 
-						chrome.tabs.update(tabId, {url: newUrl});
-						return false;
-					}
-				});
-			});
-		}
+		// 				chrome.tabs.update(tab.id, {url: newUrl});
+		// 				return false;
+		// 			}
+		// 		});
+		// 	});
+		// }
 	}
 }
 
--- google-chrome-domain-swap/storage.js
+++ google-chrome-domain-swap/storage.js
@@ -3,29 +3,14 @@ import { DomainSet } from './domain-sets.js';
 var Storage = {
 	store: function (sets) {
 		chrome.storage.local.set({domain_sets: sets});
-		// localStorage.domain_sets = JSON.stringify(sets);
 	},
 
 	load: async function () {
-		var sets = {};
 		var result = await chrome.storage.local.get(["domain_sets"]);
 		if (!result || !result.domain_sets) {
 			return [];
 		}
-		// for (var i in result.domain_sets) {
-		// 	sets[i] = result.domain_sets[i];
-		// }
 		return result.domain_sets;
-		// var sets = localStorage.domain_sets;
-		// console.log(sets);
-		var result = [];
-		if( ! sets ) { return result; }
-		// sets = JSON.parse(sets);
-		for( var i in sets ) {
-			result.push(new DomainSet(sets[i].name, sets[i].domains));
-			// sets[i] = new DomainSet(sets[i].name, sets[i].domains);
-		}
-		return result;
 	}
 };
 
